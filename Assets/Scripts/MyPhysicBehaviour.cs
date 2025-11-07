9dcfc28b36ea4c38539c9e6f77084a4a63c7a647
Reza <rezdev.ir@gmail.com>
2025-11-07 19:25:40 +0330

    NEW_METHOD

--- Assets/Scripts/MyPhysicBehaviour.cs
+++ Assets/Scripts/MyPhysicBehaviour.cs
@@ -1,20 +1,25 @@
+using System;
 using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+enum PlayerState { Air_Pressed, Surface_Pressed_Good,Surface_Pressed_Bad, none, Air, Surface,InDeepWater}
 public class MyPhysicBehaviour : MonoBehaviour
 {
     Rigidbody2D rg;
     Collider2D cl;
-
+    PlayerState state=PlayerState.Air;
     InputSystem_Actions PlayerInput;
     [SerializeField] float GoDeepDist = 1f;
     [SerializeField] float GoHighDist = 1f;
     [SerializeField] float Splash_Force = 2f;
-    [SerializeField] float JumpForce=2f;
+    [SerializeField] float JumpForce = 2f;
+
+    [SerializeField] float Rotaion_Speed = 100f;
     [SerializeField] float2 RotationLimit;
-     [SerializeField] float2 PlayerLimit;
+    [SerializeField] float2 PlayerLimit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -31,8 +36,8 @@ public class MyPhysicBehaviour : MonoBehaviour
     {
 
     }
-    
-        void CancelClick(InputAction.CallbackContext a)
+
+    void CancelClick(InputAction.CallbackContext a)
     {
         rg.linearVelocity = Vector2.zero;
     }
@@ -48,13 +53,43 @@ public class MyPhysicBehaviour : MonoBehaviour
 
     void Update()
     {
-          Jumping();
+        //   Jumping();
+        ClickManager();
     }
+
+    void BoardRotation(float angle)
+    {
+        float deg = Mathf.MoveTowardsAngle(transform.eulerAngles.z, angle, Time.deltaTime * Rotaion_Speed);
+        transform.rotation = Quaternion.Euler(0, 0, deg);
+
+    }
+
+    void ClickManager()
+    {
+        if (PlayerInput.UI.Click.IsPressed())
+        {
+            if(state==PlayerState.Air)
+            {
+                // rg.linearVelocityY -= 5;
+                state = PlayerState.Air_Pressed;
+                
+            }
+
+        }
+        else
+        {
+             if(state==PlayerState.Air_Pressed)
+            {
+                state = PlayerState.Air;
+            }
+        }
+
+    }
+
 
     void MakeConstrain()
     {
-        // transform.eulerAngles = new Vector3(0, 0,
-        // Mathf.Clamp(transform.eulerAngles.z, RotationLimit.x, RotationLimit.y));
+
 
         float z = transform.eulerAngles.z;
 
@@ -66,89 +101,103 @@ public class MyPhysicBehaviour : MonoBehaviour
 
         // Apply rotation back
         transform.rotation = Quaternion.Euler(0, 0, z);
-        // Vector2 pos = rg.position;
-        // pos.x = Mathf.Clamp(pos.x, PlayerLimit.x, PlayerLimit.y);
+
+
+
+        Vector2 pos = rg.position;
+
+        // Clamp X position
+        if (pos.x < PlayerLimit.x)
+        {
+            pos.x = PlayerLimit.x;
+            rg.linearVelocity = new Vector2(0, rg.linearVelocity.y); // stop going outside
+        }
+        else if (pos.x > PlayerLimit.y)
+        {
+            pos.x = PlayerLimit.y;
+            rg.linearVelocity = new Vector2(0, rg.linearVelocity.y);
+        }
+
         // rg.MovePosition(pos);
-        // transform.position=new Vector3(0, 0,
-        // Mathf.Clamp(transform.position.x, PlayerLimit.x, PlayerLimit.y));
-
-
-            Vector2 pos = rg.position;
-
-    // Clamp X position
-    if (pos.x < PlayerLimit.x)
-    {
-        pos.x = PlayerLimit.x;
-        rg.linearVelocity = new Vector2(0, rg.linearVelocity.y); // stop going outside
-    }
-    else if (pos.x > PlayerLimit.y)
-    {
-        pos.x = PlayerLimit.y;
-        rg.linearVelocity = new Vector2(0, rg.linearVelocity.y);
-    }
-
-    // rg.MovePosition(pos);
 
 
     }
 
     Vector2 water_surfece_position;
 
-
-    bool touch_surface = false;
-    bool reach_deppest = false;
-    bool reach_highest = false;
-
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "ocean")
         {
-            if (!touch_surface)
+            if (state == PlayerState.Air)
             {
                 Debug.Log("On Splash");
                 water_surfece_position = transform.position;
-                touch_surface = true;
+                state = PlayerState.Surface;
 
             }
+ 
 
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+
+    float surfaceAngle; // store last slope angle
+
+    void OnCollisionStay2D(Collision2D collision)
     {
-
-        if (collision.gameObject.tag == "ocean")
+        // Check if we're colliding with the wave
+        if (collision.collider.CompareTag("ocean"))
         {
-            if (!touch_surface)
-            {
-                Debug.Log("Float on Water");
-                touch_surface = false;
-                reach_deppest = false;
-                reach_highest = false;
+            // Get first contact point
+          
+            ContactPoint2D contact = collision.GetContact(0);
+            Debug.Log("Get Slope Stay:" + GetSlop(contact));
 
-            }
 
+
+
+        }
+    }
+
+    bool GetSlop(ContactPoint2D contact)
+    {
+        Vector2 normal = contact.normal;
+
+        // Convert normal to surface angle
+        surfaceAngle = Mathf.Atan2(normal.x, normal.y) * Mathf.Rad2Deg;
+        // Debug.Log(surfaceAngle);
+        if (surfaceAngle < 0)
+        {
+            Debug.Log("Up Hill");
+            // IsUpHill = true;
+            return true;
+        }
+        else
+        {
+            // IsUpHill = false;
+            Debug.Log("Down Hill");
+            return false;
         }
     }
 
     void GoDeep()
     {
-        if (touch_surface && !reach_deppest)
+        if (state == PlayerState.Surface )
         {
+
             if (Mathf.Abs(Vector2.Distance(water_surfece_position, transform.position)) >= GoDeepDist)
             {
                 //Reach Deepest section 
                 // Now 
                 Debug.Log("Reach Deepest");
                 rg.linearVelocity = Vector2.zero;
-                reach_deppest = true;
+                state = PlayerState.InDeepWater;
                 StartCoroutine(CoolDown_Splash());
                 rg.AddForceY(Splash_Force);
-
-
             }
         }
+    
     }
 
     bool Sp_coolDown = true;
@@ -160,25 +209,24 @@ public class MyPhysicBehaviour : MonoBehaviour
     }
     void GoUp()
     {
-        if (reach_deppest && !reach_highest)
+        if (state == PlayerState.InDeepWater)
         {
             if (Sp_coolDown)
             {
-                //Reach Deepest section 
+                //Reach Highest section 
                 // Now 
-                Debug.Log("Reach Highest");
+                state = PlayerState.Air;
                 rg.linearVelocity = Vector2.zero;
                 cl.isTrigger = false;
-                reach_highest = true;
-
-
-
-
+                
             }
         }
     }
 
-
+    void Surffing()
+    {
+        //This Act Just when Pressed click
+    }
     public void Jumping()
     {
         if (PlayerInput.UI.Click.IsPressed())
@@ -186,10 +234,8 @@ public class MyPhysicBehaviour : MonoBehaviour
             Debug.Log("PRESSED");
             rg.AddForceY(JumpForce);
             rg.AddForceX(JumpForce / 2);
-           
+
         }
-   
-          
     }
 
 }
